405b5ae5aba976c0894f5f54c7e7f53887950707
Matthew Conway <mattreduce@users.noreply.github.com>
2021-02-16 17:00:09 -0800

    Start using Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "InjectCheck",
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "InjectCheck",
+            dependencies: [])
+    ]
+)
